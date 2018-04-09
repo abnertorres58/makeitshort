2e0d8b6f46f96e11c2d44dd8274a24e892b85303
Freud Romero <abnertorres58@gmail.com>
2018-04-09 03:14:05 -0400

    basic docker recipe - for docker-compose up - config file

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ var config = {};
 config.db = {};
 config.webhost = 'http://localhost:3000/';
 
-config.db.host = 'localhost';
+config.db.host = 'mongo2:27017';
 config.db.name = 'short_urls';
 
 module.exports = config;

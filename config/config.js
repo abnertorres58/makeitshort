2d414349f12f47d157b7bdc7905a78790ab2a823
Freud Romero <abnertorres58@gmail.com>
2018-04-11 02:49:59 -0400

    set config to localhost without docker.

--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@ var config = {};
 config.db = {};
 config.webhost = 'http://localhost:3000/';
 
-config.db.host = 'mongo2:27017';
+//config.db.host = 'mongo2:27017'; // in case of mongo running with docker-compose up
+config.db.host = 'localhost:27017'; // in case of running npm start and mongo local to mac
 config.db.name = 'short_urls';
 
 module.exports = config;
